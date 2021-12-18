f37f26317e600b68912b709099ca5a5ab82d36c3
idris <idris.sabanli@gmail.com>
2021-12-19 01:00:50 +0400

    socket connection worked

--- LabrinFSH/asgi.py
+++ LabrinFSH/asgi.py
@@ -1,29 +1,21 @@
-"""
-ASGI config for chat project.
-It exposes the ASGI callable as a module-level variable named ``application``.
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
-"""
-
-import os
-import django
-from django.urls import re_path
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.routing import ProtocolTypeRouter
+from channels.routing import URLRouter
+from django.conf.urls import url
 from channels.auth import AuthMiddlewareStack
-
+from channels.security.websocket import AllowedHostsOriginValidator, OriginValidator
+from django.urls import path
 from core.consumers import ChatConsumer
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LabrinFSH.settings')
-django.setup()
-
-
 application = ProtocolTypeRouter({
-  "http": get_asgi_application(),
-  "websocket": AuthMiddlewareStack(
-        URLRouter([
-            re_path(r"^(?P<username>[\w.@+-]+)", ChatConsumer),
-        ])
-    ),
+    # Empty for now (http->django views is added by default)
+    'websocket': AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                [
+                    path('detail/<int:file_id>', ChatConsumer.as_asgi())
+                ]
+            )
+        )
+    )
 })
